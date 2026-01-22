203aa1ed170338f8219752d3310797f1aff7f514
Zahu <fathimasahra294@gmail.com>
2026-01-22 09:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
-app = Flask(__name__, template_folder="webui", static_folder="static")
+app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def home():
-    return render_template("index.html")
+    return "Hello from Flask on ECS Fargate!\n"
+
+@app.get("/health")
+def health():
+    return jsonify(status="ok"), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    # Important: listen on 0.0.0.0 and port 8080
+    app.run(host="0.0.0.0", port=8080)
+
